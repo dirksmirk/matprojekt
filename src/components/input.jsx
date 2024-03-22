6826198ae63dbf49e030b2a10175af0df08fe31b
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-03-22 13:43:16 +0100

    Getting info from API works

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,45 +1,29 @@
-import { useState, useEffect /* useRef */ } from 'react'
+import { useState, /* useEffect */ useRef } from 'react'
 
 function Search() {
     const [Food, setFood] = useState(null)
 
-  /*   const input = useRef() */
-
-    useEffect(() => {
-      fetch('https://www.themealdb.com/api/json/v1/1/search.php?s=pilaf')
+    const input = useRef()
+    
+    const getFood = () => {
+      const name = input.current.value
+        fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
         .then((response) => response.json())
         .then(result => {
           console.log(result)
           setFood(result)
         })
-    }, [])
+    }
     
-    /* const getFood = () => {
-      const name = input.current.value
-        fetch(`www.themealdb.com/api/json/v1/1/search.php?s=pilaf`)
-        .then((response) => response.json())
-        .then((result) => setFood(result))
-    } */
-    
-    // return (
-    //   <>
-    //       {/* Display selected post if exists */}
-    //       {Food &&
-    //         <div className="card">
-    //           <p>Current Food title: {Food.title}</p>
-    //           <p>Current Food body: {Food.body}</p>
-    //         </div>
-    //       }
-    //       {/* Input for entering post ID */}
-    //       <input type="text" onInput={getFood} ref={input} placeholder="Enter an ID" />
-    //       <button  >Search</button>
-    //       <br /><br />
-    //       {/* Render mock data if available */}
-          
-    //     </>
-    //   )
+     return (
+       <>
+           {/* Input for entering food name, input is gathered in the input field but activates when clicking the button */}
+           <input type="text" /* onInput={getFood} */ ref={input} placeholder="Enter an ID" />
+           <br />
+           <button onClick={getFood}>Search</button>
+         </>
+       )
       }
-
 
 
 export default Search
