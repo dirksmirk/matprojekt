dc9693ef8763de6d0ae7f2bb8c923de2f7dc83b4
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-03-24 23:59:01 +0100

    Automatic ingredient list working

--- src/components/selectedMeal.jsx
+++ src/components/selectedMeal.jsx
@@ -1,13 +1,26 @@
 /* eslint-disable react/prop-types */
 const MealMenu = ({ Recipe }) => {
 
+    const ingredients = Object.keys(Recipe) // eller vad ditt objekt nu heter
+    .filter(key => key.startsWith('strIngredient') && Recipe[key])
+    .map(key => Recipe[key]);
+
+    console.log(ingredients)
     return (
         <>
                 <div className='meal-recipe' key={Recipe.idMeal}>
                   <h2>{ Recipe.strMeal }</h2><br />
                   <img src={Recipe.strMealThumb} alt="picture of food" /><br />
                   <h3>Ingredients:</h3><br />
-                  <p>Contains: { Recipe.strIngredient1 }</p>
+                  <ul>
+                    {
+                        ingredients.map((ingredient, idx) => (
+                            <li key={idx}>
+                                <p>Contains: {ingredient}</p>
+                            </li>
+                        ))
+                    }
+                  </ul>
 
                 </div>
         </>
