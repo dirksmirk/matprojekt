be3fb42ab48f6fcc0e53f74ff0905fd1f509fe8b
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-19 13:01:04 +0200

    material UI implementation

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -1,8 +1,8 @@
 import { useContext, useRef } from "react";
 import { SearchContext } from "../Context";
+import { Button, TextField } from '@mui/material';
 
 const Header = () => {
-    
     const { myUpdateFunc } = useContext(SearchContext);
     const input = useRef();
   
@@ -16,8 +16,8 @@ const Header = () => {
             <h1 className="webHead">Great recipes</h1>
             <p>So many great recipes all in one place!</p>
             <p>Please search and look around to find your favorite one</p>
-            <input type="text" ref={input} placeholder="Enter an ID" />
-            <button onClick={handleSearch}>Search</button>
+            <TextField id="outlined-basic" label="Search" variant="outlined" size="small" inputRef={input} placeholder="Search for a recipe!" />
+            <Button onClick={handleSearch} variant="contained" size="medium">Search</Button>
         </div>
         
     )
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from 'react'
 import { SearchContext } from '../Context'
 import List from './list'
 import MealMenu from './selectedMeal'
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 //Våran function för input, som för tillfället hanterar sökrutan och fetch, sedan visar våran lista av recept samt visar receptet när man klickar på ett i listan
 function Input() {
     //Vi börjar med att sätta några variabler här i början. useState använder vi för dynamisk data 
@@ -28,20 +29,16 @@ function Input() {
     }, [myValue])
 
      return (
-       <>
-        <div className='meal-container'>
-            {/* Input för att samla användarens sök, man klickar sedan på knappen för att aktivera och köra fetch */}
-            <br />
+        <Grid container columnSpacing={4} width='95%' height='95%' className='meal-container'>
             {/* Dessa två divar rendererar våran lista av recept samt visar vårat recept. Food && och Recipe && gör så att dem också endast rendererar när Food och Recipe har ett värde */}
-            <div className="Food-List">
+            <Grid xs={4} height='720px' overflow='auto' className="Food-List">
               {/* handleRecipeClick använder vi för att skicka vidare setRecipe till våran list komponent, som  vi i sin tur använder för att skicka datan till våran MealMenu för att visa individuella recept */}
               {Food && <List Food={Food} handleRecipeClick={setRecipe} title="all food" />}
-            </div>
-            <div className='recipe-display'>
+            </Grid>
+            <Grid xs={8} height='720px' overflow='auto' className='recipe-display'>
               {Recipe && <MealMenu Recipe={Recipe} title="recipe" /> }
-            </div>
-           </div>
-         </>
+            </Grid>
+           </Grid>
        );
       }
 
