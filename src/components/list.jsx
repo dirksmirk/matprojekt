52dc9217ffabf2daf1aab72d57bbb43f6110f854
Dirk Hubrecht <dirk.holger.hubrecht@gmail.com>
2024-04-19 16:44:48 +0200

    List made with MUI

--- src/components/list.jsx
+++ src/components/list.jsx
@@ -1,13 +1,18 @@
+import { Card, CardContent, CardMedia, Typography  } from "@mui/material";
 /* eslint-disable react/prop-types */
 const List = ({ Food, handleRecipeClick }) => {
 
     return (
         <>
             {Food.map((meal) => (
-                <div className='meal-list-preview' key={meal.idMeal}  onClick={() => handleRecipeClick(meal)} >
-                  <img src={meal.strMealThumb} alt="picture of food" />
-                  <h2>{ meal.strMeal }</h2>
-                </div>
+                <Card sx={{ m: 5, p: 1, mx: 'auto', backgroundColor: 'beige' }} className='meal-list-preview' key={meal.idMeal}  onClick={() => handleRecipeClick(meal)} >
+                  <CardMedia component="picture" sx={{ width: '200%', mx: 'auto'}}>
+                    <img src={meal.strMealThumb} alt="picture of food" />
+                  </CardMedia>
+                  <CardContent>
+                    <Typography variant="h2">{ meal.strMeal }</Typography>
+                  </CardContent>
+                </Card>
               ))
             }
         </>
